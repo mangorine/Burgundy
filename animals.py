98bfcc5aad8e9daf9852c61c848b1a8526d133c6
Pulvsama <idreaqz12345@hotmail.com>
2025-11-24 13:18:32 +0100

    Notebook to python

---
+++ animals.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class AnimalType(Enum):
+    CATTLE = "cattle"
+    CHICKEN = "chicken"
+    GOAT = "goat"
+    PIG = "pig"
+    SHEEP = "sheep"
+
+class Animal:
+    def __init__(self, type: AnimalType, quantity: int, color: Color) -> None:
+        self.type = type
+        self.quantity = quantity
+        self.color = color
