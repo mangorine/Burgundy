67f4a3f3374d33e1041b827de48ae7c9ae5fb8f4
mangorine <clement.lin@eleves.enpc.fr>
2026-01-12 14:11:34 +0100

    changed colors in playerboard

--- colors.py
+++ colors.py
@@ -3,11 +3,11 @@
 from board import TileType
 
 TILE_COLORS = {
-    TileType.CASTLE:   (200, 200, 200),   # gris clair
-    TileType.BUILDING: (60, 120, 220),    # bleu
-    TileType.ANIMAL:   (90, 170, 90),     # vert
+    TileType.CASTLE:   (0, 86, 27),   # vert fonce
+    TileType.BUILDING: (136, 66, 29),    # marron
+    TileType.ANIMAL:   (0, 255, 0),     # vert
     TileType.MINE:     (70, 70, 70),       # gris foncé
-    TileType.KNOWLEDGE:(170, 110, 170),    # violet
+    TileType.KNOWLEDGE:(255, 255, 0),    # jaune
     TileType.SHIP:     (100, 190, 210),    # cyan
 }
 
